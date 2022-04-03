c8d1aaac96bdf1d303c9de19af0712d5aa8cb546
Your Name <you@example.com>
2022-04-04 03:13:22 +0600

    three review shown in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useReviews from '../../Hooks/useReviews';
 import bike from '../../Resources/bike.jpg'
 import './Home.css'
@@ -8,6 +8,10 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [reviews, setReviews] = useReviews();
+    if (reviews.length > 3) {
+        const newreviews = reviews.slice(0, 3)
+        setReviews(newreviews)
+    }
     return (
         <div>
             <div className='home-container'>
@@ -17,12 +21,12 @@ const Home = () => {
                     <h1>Honda CB150R Streetfire</h1>
                     <h3>Can Be Your next Dream Bike</h3>
                     <p>Honda CB150R Streetfire is one of the top rated Honda's racing bikes, which is the member of world famous CB150R naked sport bike series. The CB150R Streetfire is also made by PT Astra Honda Motor one of the subsidiaries of Honda Global. The bike was made in Indonesia for south Asian markets including Bangladesh.</p>
-                    <button className='btn'>See More</button>
+                    <button className='btn'> See More</button>
                 </div>
             </div>
 
             <div customer-review>
-                <h1>Customer review :{reviews.length}</h1>
+                <h1>Customer Review</h1>
                 <div className='review-container'>
                     {
                         reviews.map(review => <SingleReview
