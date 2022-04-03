360054c7f24a71ad98690cc8727f42f202b7f21b
Your Name <you@example.com>
2022-04-04 02:22:27 +0600

    review showen in the review page

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useReviews from '../../Hooks/useReviews';
+import SingleReview from '../SingleReview/SingleReview';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useReviews()
     return (
         <div>
-            this is from review
+            <h1> What Our Valuable Customer Say !!!</h1>
+            <div className='review-container'>
+                {
+                    reviews.map(review => <SingleReview
+                        key={review.id}
+                        review={review}
+                    ></SingleReview>)
+                }
+
+            </div>
         </div>
     );
 };
