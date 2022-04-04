5fca0a74641b85d341d3e9d3b57a40328ef11e22
Your Name <you@example.com>
2022-04-04 14:54:36 +0600

    more chat added

--- src/Components/Dasboard/Dasboard.js
+++ src/Components/Dasboard/Dasboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import LineCharts from '../LineCharts/LineCharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dasboard.css'
 
 const Dasboard = () => {
     const data = [
@@ -41,11 +42,50 @@ const Dasboard = () => {
         }
     ]
     return (
-        <div>
-            <LineCharts
-                key={data.month}
-                data={data}
-            ></LineCharts>
+        <div className='chart-container'>
+            <div>
+                <LineChart width={600} height={300} data={data} >
+                    <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
+                    <XAxis dataKey='month'></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                    <Legend verticalAlign="bottom" height={36}></Legend>
+                    <Line name='Month Wise Sell' type="monotone" dataKey="sell" stroke="#8884d8" />
+                </LineChart>
+            </div>
+
+            <div>
+                <AreaChart width={600} height={300} data={data}>
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Legend verticalAlign="bottom" height={36} />
+                    <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </div>
+            <div>
+                <BarChart width={600} height={300} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </div>
         </div>
     );
 };
